df0c1b8ffb57e51027a27218ef76e29431bdf942
= <=>
2025-07-30 16:13:37 +0900

    메뉴판 영역 제작

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -1,19 +1,26 @@
+// EmailJS 초기화
+(function() {
+    emailjs.init("GOGAKbWy6UaHH4dxK"); // 여기에 실제 Public Key를 입력하세요
+})();
+
 jQuery(document).ready(function($) {
   "use strict";
 
-  //Contact
-  $('form.contactForm').submit(function() {
+  //Contact Form with EmailJS
+  $('form.contactForm').submit(function(e) {
+    e.preventDefault(); // 기본 form 제출 막기
+    
     var f = $(this).find('.form-group'),
-      ferror = false,
-      emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
+        ferror = false,
+        emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
 
-    f.children('input').each(function() { // run all inputs
-
-      var i = $(this); // current input
+    // 입력 필드 검증
+    f.children('input').each(function() {
+      var i = $(this);
       var rule = i.attr('data-rule');
 
       if (rule !== undefined) {
-        var ierror = false; // error flag for current input
+        var ierror = false;
         var pos = rule.indexOf(':', 0);
         if (pos >= 0) {
           var exp = rule.substr(pos + 1, rule.length);
@@ -42,7 +49,7 @@ jQuery(document).ready(function($) {
             break;
 
           case 'checked':
-            if (! i.is(':checked')) {
+            if (!i.is(':checked')) {
               ferror = ierror = true;
             }
             break;
@@ -57,13 +64,14 @@ jQuery(document).ready(function($) {
         i.next('.validation').html((ierror ? (i.attr('data-msg') !== undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
       }
     });
-    f.children('textarea').each(function() { // run all inputs
 
-      var i = $(this); // current input
+    // textarea 검증
+    f.children('textarea').each(function() {
+      var i = $(this);
       var rule = i.attr('data-rule');
 
       if (rule !== undefined) {
-        var ierror = false; // error flag for current input
+        var ierror = false;
         var pos = rule.indexOf(':', 0);
         if (pos >= 0) {
           var exp = rule.substr(pos + 1, rule.length);
@@ -88,31 +96,32 @@ jQuery(document).ready(function($) {
         i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
       }
     });
-    if (ferror) return false;
-    else var str = $(this).serialize();
-    var action = $(this).attr('action');
-    if( ! action ) {
-      action = 'contactform/contactform.php';
-    }
-    $.ajax({
-      type: "POST",
-      url: action,
-      data: str,
-      success: function(msg) {
-        // alert(msg);
-        if (msg == 'OK') {
-          $("#sendmessage").addClass("show");
-          $("#errormessage").removeClass("show");
-          $('.contactForm').find("input, textarea").val("");
-        } else {
-          $("#sendmessage").removeClass("show");
-          $("#errormessage").addClass("show");
-          $('#errormessage').html(msg);
-        }
 
-      }
-    });
+    // 검증 실패시 전송 중단
+    if (ferror) return false;
+
+    // EmailJS로 이메일 전송
+    var form = this;
+    
+    // 로딩 표시
+    $('#sendmessage').removeClass('show');
+    $('#errormessage').removeClass('show');
+    
+    emailjs.sendForm('service_9qzrs8c', 'template_g7gcob8', form)
+      .then(function() {
+        // 성공
+        $("#sendmessage").addClass("show");
+        $("#errormessage").removeClass("show");
+        $('.contactForm').find("input, textarea").val("");
+        console.log('이메일 전송 성공!');
+      }, function(error) {
+        // 실패
+        $("#sendmessage").removeClass("show");
+        $("#errormessage").addClass("show");
+        $('#errormessage').html('이메일 전송에 실패했습니다. 다시 시도해주세요.');
+        console.log('이메일 전송 실패:', error);
+      });
+
     return false;
   });
-
 });
